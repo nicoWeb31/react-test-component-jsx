bdb85716e09f65ae5ea3ad0e817ad93f2a4856f8
nicoWeb31 <nico.riot@free.fr>
2020-09-15 14:42:53 +0200

    add sementic ui

--- src/index.js
+++ src/index.js
@@ -3,14 +3,30 @@ import ReactDom from 'react-dom';
 
 const App = () => {
     return (
-        <div className="">
-toto
+        <div className="ui container comments" >
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                </a>
+                    <div className="metadata">
+                        <span className='date'>Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+                </div>
+            </div>
+
         </div>
     );
 }
 
 
 ReactDom.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 )
