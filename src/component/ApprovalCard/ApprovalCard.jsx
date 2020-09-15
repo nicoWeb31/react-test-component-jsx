744f1d668443308e57ec0d8c0c84b73a24561ebb
nicoWeb31 <nico.riot@free.fr>
2020-09-15 21:59:30 +0200

    class test

--- src/component/ApprovalCard/ApprovalCard.jsx
+++ src/component/ApprovalCard/ApprovalCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-
-const ApprovalCard = ({ image }) => {
+//children inside tag
+const ApprovalCard = ({ children }) => {
     return (
-        <div className="card">
+        <div className="ui card">
             <div className="content">
-        
-    Are you sure ?
-    </div>
+
+                {children}
+            </div>
             <div className="extra content">
                 <div className="ui two buttons">
                     <div className="ui basic green button">Approve</div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
 import CommentItems from './component/Comment/CommentItems';
+import ApprovalCard from './component/ApprovalCard/ApprovalCard';
 
 const App = () => {
 
@@ -9,10 +10,29 @@ const App = () => {
 
     return (
         <div className="ui container comments" >
+        
+        <ApprovalCard>
+        <div>
+            <h4>
+            Warning !
+            </h4>
+            Are you want to do this ?
+        </div>
+        </ApprovalCard>
 
-            <CommentItems name='sam' content="Nice Blog post" image={faker.image.avatar()} date='today at 6:00' />
-            <CommentItems name='alex' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
-            <CommentItems name='jane' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
+
+            <ApprovalCard>
+                <CommentItems name='sam' content="Nice Blog post" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentItems name='alex' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentItems name='jane' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
 
 
         </div>
