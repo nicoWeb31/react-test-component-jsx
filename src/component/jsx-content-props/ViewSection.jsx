e4cb213cab0340c52e6be728397a91685d512528
nicoWeb31 <nico.riot@free.fr>
2020-09-15 22:05:34 +0200

    refonte arbo

---
+++ src/component/jsx-content-props/ViewSection.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard/ApprovalCard';
+import CommentItems from './Comment/CommentItems';
+
+
+const ViewSection = () => {
+    return (
+        <div className="ui container comments" >
+        
+        <ApprovalCard>
+        <div>
+            <h4>
+            Warning !
+            </h4>
+            Are you want to do this ?
+        </div>
+        </ApprovalCard>
+
+
+            <ApprovalCard>
+                <CommentItems name='sam' content="Nice Blog post" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentItems name='alex' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentItems name='jane' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
+            </ApprovalCard>
+
+
+
+        </div>
+    );
+}
+
+export default ViewSection;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import faker from 'faker';
-import CommentItems from './component/Comment/CommentItems';
-import ApprovalCard from './component/ApprovalCard/ApprovalCard';
+import ViewSection from './component/jsx-content-props/ViewSection';
 
 const App = () => {
 
 
 
     return (
-        <div className="ui container comments" >
-        
-        <ApprovalCard>
-        <div>
-            <h4>
-            Warning !
-            </h4>
-            Are you want to do this ?
-        </div>
-        </ApprovalCard>
-
-
-            <ApprovalCard>
-                <CommentItems name='sam' content="Nice Blog post" image={faker.image.avatar()} date='today at 6:00' />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentItems name='alex' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentItems name='jane' content="lorem ksksksksk" image={faker.image.avatar()} date='today at 6:00' />
-            </ApprovalCard>
-
-
-
-        </div>
+<>
+    <ViewSection/>
+</>
     );
 }
 
