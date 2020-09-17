70b46f14de55c9709d1cca47e89dff37afa5250c
nicoWeb31 <nico.riot@free.fr>
2020-09-17 18:19:25 +0200

    add icon

--- src/component/section-Class-component/SeasonDisplay.jsx
+++ src/component/section-Class-component/SeasonDisplay.jsx
@@ -1,28 +1,33 @@
 
 import React from 'react';
 
-const getSeason =(lat,month)=>{
-    if( month > 2 && month > 9 ){
+const getSeason = (lat, month) => {
+    if (month > 2 && month > 9) {
         return lat > 0 ? "summer" : "winter"
-    }else{
+    } else {
         return lat < 0 ? "winter" : "summer"
     }
 }
 
 
-const SeasonDisplay = ({lat}) => {
+const SeasonDisplay = ({ lat }) => {
 
     //month js de 0 - 11
     //northern 0 1 3 9 10 11 winter
     //southern 3 4 5 6 7 8 winter
     const currentMonth = new Date().getMonth();
+    const season = getSeason(lat, currentMonth);
+    const text = season === 'winter' ? "Burr, it is chily" : "lets hit the beach"
+    const icon = season === 'winter' ? "snowflake" : "sun"
 
-    
+
     return (
         <div>
-            {
-                getSeason(lat,currentMonth)
-            }
+            <i class={`${icon} icon`}></i>
+            <h1>
+                {text}
+            </h1>
+            <i class={`${icon} icon`}></i>
         </div>
     );
 }
