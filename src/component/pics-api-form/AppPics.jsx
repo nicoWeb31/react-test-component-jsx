ce779ea9ebf7c93a8c92b50dadc734b209138957
nicoWeb31 <nico.riot@free.fr>
2020-09-18 19:10:49 +0200

    fetch data from unsplash api

--- src/component/pics-api-form/AppPics.jsx
+++ src/component/pics-api-form/AppPics.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import ImgList from './image-list/ImgList';
 import SearchBar from './search/SearchBar';
+import axios from 'axios'
 
 import "./AppPic.style.css"
 
 
 const AppPics = () => {
+
+
+    //unsplash api photo
+    const apiKey = 'VFAuFBpe9cSdkBDfOQ-No7A2nYz0cv3fape2F0m9Mr8'
+    const secretKey = 'IlXNU1fKKLFRMRkj60GW_eFL-5zIa8iVpCB22Vpsld0'
 
 
     const [serchValue, setSearchValue] = useState('')
