d451f4ce32cec812d1f6dc8a3070b81d82b12ba3
nicoWeb31 <nico.riot@free.fr>
2020-09-20 12:10:34 +0200

    render with active index

--- src/component/widget-hook/accordeon/Accordeon.jsx
+++ src/component/widget-hook/accordeon/Accordeon.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 
-const Accordeon = () => {
+const Accordeon = ({ items }) => {
+
+    const [activeId, setActiveId] = useState(null)
+
+    const handleClick = (id) =>{
+        setActiveId(id)
+    }
+
+
+    //map render item
+    const renderItems = items.map((item,index) => {
+        return (
+            <Fragment key={item.title}>
+                <div className="title active"  onClick={()=>handleClick(index)}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+            {
+                activeId === index ? 
+                (
+                    item.content
+                ):
+                ('')
+            }
+
+            </Fragment>
+        )
+    })
+
+
+
     return (
         <>
-            <h1>Accordion</h1>
+            <div className ="ui styled accordion">
+                {renderItems}
+            </div>
+            {activeId}
         </>
     );
 }
