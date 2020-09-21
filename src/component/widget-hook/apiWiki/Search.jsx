54c4c2aabd1c1b62cc9b47213e72839aa3ca2cf6
nicoWeb31 <nico.riot@free.fr>
2020-09-21 19:39:27 +0200

    use Effect

--- src/component/widget-hook/apiWiki/Search.jsx
+++ src/component/widget-hook/apiWiki/Search.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
 
 
 
 const Search = () => {
+
+
+
+    useEffect(() =>{
+
+    },[])
+
+    const [term, setTerm] = useState("")
+
+    const onInputChange = (e) =>{
+        const {value} = e.target;
+        setTerm(value)
+    }
+
     return (
         <>
             <h1>Search </h1>
-            <label> Search : </label>
-            <input />
+            <div className="ui form">
+                <div className="field">
+                <label>Enter search term : </label>
+                <input className='input' onChange={onInputChange} value={term} name="term"/>
+
+
+                {term}
+
+
+                </div>
+            </div>
+
 
         </>
     );
