e361bd97c6a04ec49d51b2a779c0ec18c256b76d
nicoWeb31 <nico.riot@free.fr>
2020-09-22 17:33:09 +0200

    useRef

--- src/component/widget-hook/list/List.jsx
+++ src/component/widget-hook/list/List.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
 const List = ({ options, label,selected,onSeletedChange }) => {
 
+    const [open,setOpen] = useState(false)
+    const RefDomDropDown = useRef()
 
+
+    // empty array run ontime at the mount component
+    useEffect(()=>{
+        document.body.addEventListener('click', ({target})=>{
+
+            //si on click dans l'element auquel on fait reference ou fait rien
+            if(RefDomDropDown.current.contains(target)) return
+
+            setOpen(false)
+                
+            
+        })
+    },[])
 
     const onClickChange = (option) =>{
-        onSeletedChange(option)
+        onSeletedChange(option);
+        setOpen(false)
+    }
+
+
+    const onClickSetOpen =()=>{
+        setOpen(!open)
     }
 
     const renderOption = options.map((option) => {
+
+        if(option.value === selected.value){
+            return null;
+        }
+
         return (
             <div 
             className="item" 
@@ -24,18 +50,21 @@ const List = ({ options, label,selected,onSeletedChange }) => {
         )
     })
 
+    //ref.current ------------//
+    //console.log(RefDomDropDown.current)
+
     return (
         <>
             <h1>Liste - option - color</h1>
-            <div className="ui form">
+            <div className="ui form" style={{backgroundColor: selected.value}} ref={RefDomDropDown}>
                 <div className="field">
                     <label className='label'>
                         {label}
                     </label>
-                    <div className="ui selection dropdown visible active">
+                    <div className={(open && "visible active") + " ui selection dropdown"} onClick={onClickSetOpen} >
                         <i className="dropdown icon"></i>
                         <div className="text">{selected.label}</div>
-                        <div className="menu visible transition">
+                        <div className={`${open && 'visible transition'} menu`}>
                             {renderOption}
                         </div>
                     </div>
