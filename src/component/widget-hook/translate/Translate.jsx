a71b360cf48c2ebfef7fc7027c460b3c3206417d
nicoWeb31 <nico.riot@free.fr>
2020-09-22 19:42:00 +0200

    translate input ok

--- src/component/widget-hook/translate/Translate.jsx
+++ src/component/widget-hook/translate/Translate.jsx
@@ -8,12 +8,30 @@ import { translateOpions } from '../datas'
 
 const Translate = () => {
 
-    const [langage,setlangage] = useState(translateOpions[0])
+    //----state---//
+    const [langage, setlangage] = useState(translateOpions[0])
+    const [text, settext] = useState("")
+
+
+    //handleChange
+    const onChangeText = (e) => {
+        settext(e.target.value)
+    }
+
 
     return (
         <>
             <h1>Trtanslate</h1>
-            <List options={translateOpions} selected={langage} onSeletedChange={setlangage} />
+            <div className="ui form">
+                <div className="field">
+                    <label> Enter text</label>
+                    <input onChange={onChangeText} value={text} />
+                </div>
+
+            </div>
+            <List options={translateOpions} selected={langage} onSeletedChange={setlangage} label='Select a language !' />
+
+            {text}
         </>
     );
 }
