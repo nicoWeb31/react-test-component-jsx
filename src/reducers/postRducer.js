c2f1b80b07676c9d55eb75ea4594d7f76e2e52fe
nicoWeb31 <nico.riot@free.fr>
2020-09-25 11:49:53 +0200

    refacto reducer

--- src/reducers/postRducer.js
+++ src/reducers/postRducer.js
@@ -1,9 +1,23 @@
 
 //reducer blog
-export const postReducer = (state,action)=>{
-    if(action.type ==='FETCH_POSTS'){
-        return action.payload
+export const postReducer = (state = [],action)=>{
+    // if(action.type ==='FETCH_POSTS'){
+    //     return action.payload
+    // }
+
+    // return state
+
+
+    switch(action.type){
+        case 'FETCH_POSTS' :
+            return action.payload
+    
+
+        
+        default:
+            return state     
+
     }
 
-    return state
+
 }
